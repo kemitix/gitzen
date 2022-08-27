2a7b48bf1e8bd3ed42cee855ed8ed3d87a2fe547
Paul Campbell <pcampbell@kemitix.net>
2022-08-27 15:34:28 +0100

    Split gz-status into a launcher and an implementation
    commit-id:a1c207ff

--- gitzen/status.py
+++ gitzen/status.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-
 import subprocess
 import json
-import jmespath
-
-print("`git zen status` - prototype")
+from jmespath import search as jq
 
 query = """
 query($repo_name: String!){
@@ -41,18 +37,21 @@ query($repo_name: String!){
     }
 }
 """
-print("Querying Github...")
-# GraphQL originally from https://github.com/ejoffe/spr/blob/9597afc52354db66d4b419f7ee7a9bd7eacdf70f/github/githubclient/gen/genclient/operations.go#L72
-json = json.loads(subprocess.run(['gh', 'api', 'graphql', '-F', "repo_name={repo}", '-f', f"query={query}"], stdout=subprocess.PIPE).stdout)
 
-myPRs = jmespath.search('data.viewer.repository.pullRequests.nodes', json)
-numPRs = len(myPRs)
-if numPRs == 0:
-	print('Stack is empty - no PRs found')
-	exit
+def status():
+    print("Querying Github...")
+    # GraphQL originally from https://github.com/ejoffe/spr/blob/9597afc52354db66d4b419f7ee7a9bd7eacdf70f/github/githubclient/gen/genclient/operations.go#L72
+    result = json.loads(subprocess.run(['gh', 'api', 'graphql', '-F', "repo_name={repo}", '-f', f"query={query}"], stdout=subprocess.PIPE).stdout)
 
-for pr in myPRs:
-	n = pr['number']
-	m = pr['mergeable']
-	t = pr['title']
-	print(f'PR-{n} - {m} - {t}')
+    myPRs = jq('data.viewer.repository.pullRequests.nodes', result)
+    numPRs = len(myPRs)
+    if numPRs == 0:
+        print('Stack is empty - no PRs found')
+        exit
+
+    for pr in myPRs:
+        n = pr['number']
+
+        m = pr['mergeable']
+        t = pr['title']
+        print(f'PR-{n} - {m} - {t}')
