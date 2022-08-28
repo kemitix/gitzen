119fc343e031f4dce6660497849814dca7e7a2ce
Paul Campbell <pcampbell@kemitix.net>
2022-08-28 18:29:17 +0100

    Add config.load()
    commit-id:be7c9450

--- gitzen/config.py
+++ gitzen/config.py
@@ -1,4 +1,7 @@
+from os.path import exists
 from typing import List
+
+import bios
 
 
 class GitZenConfig:
@@ -8,3 +11,35 @@ class GitZenConfig:
     def __init__(self, defaultRemoteBranch: str, remoteBranches: str) -> None:
         self.defaultRemoteBranch = defaultRemoteBranch
         self.remoteBranchNames = remoteBranches
+
+    def __eq__(self, __o: object) -> bool:
+        return (
+            self.defaultRemoteBranch == __o.defaultRemoteBranch
+            and self.remoteBranchNames == __o.remoteBranchNames
+        )
+
+    def __repr__(self) -> str:
+        return (
+            "GitZenConfig("
+            f"defaultRemoteBranch={repr(self.defaultRemoteBranch)}, "
+            f"remoteBranchNames={repr(self.remoteBranchNames)}, "
+            ")"
+        )
+
+
+defaultConfig: GitZenConfig = GitZenConfig(
+    defaultRemoteBranch="master", remoteBranches=[]
+)
+
+
+def load(dir: str):
+    configFile = f"{dir}/.gitzen.yml"
+    if exists(configFile):
+        print(f"Reading config from {configFile}")
+        gitzenYml = bios.read(configFile)
+        return GitZenConfig(
+            defaultRemoteBranch=gitzenYml["defaultRemoteBranch"],
+            remoteBranches=gitzenYml["remoteBranches"],
+        )
+    print(f"Using default config - no file: {configFile}")
+    return defaultConfig
