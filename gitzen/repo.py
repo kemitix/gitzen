3ef39499ce2343e79c2abc0c7c01625b15f2ee58
Paul Campbell <pcampbell@kemitix.net>
2022-08-28 09:12:26 +0100

    Rename workflows folder to fix typo and fix broken builds
    commit-id:85627cf3

--- gitzen/repo.py
+++ gitzen/repo.py
@@ -1,10 +1,11 @@
 from os import error
 from gitzen import git
 
+
 def localBranch() -> str:
     branches = git.branch()
     for branch in branches:
         if branch.startswith('* '):
-            return branch.removeprefix('* ')
+            return branch[2:]
     error("Can't find local branch name")
     exit
