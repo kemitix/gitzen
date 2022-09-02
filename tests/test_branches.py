46b1a8b2a165a85367dacd76005ab4df85c1b628
Paul Campbell <pcampbell@kemitix.net>
2022-09-02 22:21:14 +0100

    branches.get_remote_branch was broken
    commit-id:2795cc30

--- tests/test_branches.py
+++ tests/test_branches.py
@@ -2,7 +2,7 @@ from gitzen.branches import get_remote_branch
 from gitzen.config import Config
 
 
-def test_getRemoteBranchName_returns_default_when_no_match():
+def test_get_remote_branch_name_returns_default_when_no_match():
     """
     Tests that getRemoteBranchName returns the default remote branch from
     config if it fails to match the local branch name in the list of mapped
@@ -19,3 +19,22 @@ def test_getRemoteBranchName_returns_default_when_no_match():
     result = get_remote_branch("other", config)
     # then
     assert result == defaultBranch
+
+
+def test_get_remote_branch_name_returns_default_when_second_remote_matches():
+    """
+    Tests that getRemoteBranchName returns the default remote branch from
+    config if it fails to match the local branch name in the list of mapped
+    remote branches as listed in the repo's config.
+    """
+    # given
+    defaultBranch = "foo"
+    config = Config(
+        default_remote_branch=defaultBranch,
+        remote_branches=["baz", "other"],
+        remote="origin",
+    )
+    # when
+    result = get_remote_branch("other", config)
+    # then
+    assert result == "other"
