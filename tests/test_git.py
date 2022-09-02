eb426a0a13db09939dadd5dbc9f5cf9121978005
Paul Campbell <pcampbell@kemitix.net>
2022-09-02 21:19:31 +0100

    Add git.rebase(target)
    commit-id:462113a1

--- tests/test_git.py
+++ tests/test_git.py
@@ -5,6 +5,72 @@ from gitzen import git
 
 
 @mock.patch("subprocess.run")
+def test_branch(mock_subproc_run):
+    """
+    Test that the correct command is invoked
+    """
+    # when
+    git.branch(git.RealGitEnv())
+    # then
+    mock_subproc_run.assert_called_with(
+        [
+            "git",
+            "branch",
+            "--no-color",
+        ],
+        stdout=PIPE,
+    )
+
+
+@mock.patch("subprocess.run")
+def test_fetch(mock_subproc_run):
+    """
+    Test that the correct command is invoked
+    """
+    # when
+    git.fetch(git.RealGitEnv())
+    # then
+    gitFetch = ["git", "fetch"]
+    mock_subproc_run.assert_called_with(gitFetch, stdout=PIPE)
+
+
+@mock.patch("subprocess.run")
+def test_remote(mock_subproc_run):
+    """
+    Test that the correct command is invoked
+    """
+    # when
+    git.remote(git.RealGitEnv())
+    # then
+    mock_subproc_run.assert_called_with(
+        [
+            "git",
+            "remote",
+            "--verbose",
+        ],
+        stdout=PIPE,
+    )
+
+
+@mock.patch("subprocess.run")
+def test_rebase(mock_subproc_run):
+    """
+    Test that the correct command is invoked
+    """
+    # when
+    git.rebase(git.RealGitEnv(), "target/branch")
+    # then
+    mock_subproc_run.assert_called_with(
+        [
+            "git",
+            "rebase",
+            "target/branch",
+        ],
+        stdout=PIPE,
+    )
+
+
+@mock.patch("subprocess.run")
 def test_revParse(mock_subproc_run):
     """
     Test that the correct command is invoked
@@ -29,51 +95,3 @@ def test_revParse_returns_value(mock_subproc_run):
     result = git.revParse(git.RealGitEnv())
     # then
     assert result == ["688881f74786d59ff397ef81efe1c137167f46b2"]
-
-
-@mock.patch("subprocess.run")
-def test_fetch(mock_subproc_run):
-    """
-    Test that the correct command is invoked
-    """
-    # when
-    git.fetch(git.RealGitEnv())
-    # then
-    gitFetch = ["git", "fetch"]
-    mock_subproc_run.assert_called_with(gitFetch, stdout=PIPE)
-
-
-@mock.patch("subprocess.run")
-def test_branch(mock_subproc_run):
-    """
-    Test that the correct command is invoked
-    """
-    # when
-    git.branch(git.RealGitEnv())
-    # then
-    mock_subproc_run.assert_called_with(
-        [
-            "git",
-            "branch",
-            "--no-color",
-        ],
-        stdout=PIPE,
-    )
-
-
-@mock.patch("subprocess.run")
-def test_remote(mock_subproc_run):
-    """
-    Test that the correct command is invoked
-    """
-    # when
-    git.remote(git.RealGitEnv())
-    # then
-    mock_subproc_run.assert_called_with(
-        [
-            "git",
-            "remote",
-            "--verbose",
-        ],
-        stdout=PIPE,
-    )
