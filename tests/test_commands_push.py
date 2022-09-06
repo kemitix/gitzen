f6e1ea2477233ae25b1c367d6f0c3e9864c00d84
Paul Campbell <pcampbell@kemitix.net>
2022-09-06 21:10:39 +0100

    Add push.reordered()
    commit-id:f4184e90

--- tests/test_commands_push.py
+++ tests/test_commands_push.py
@@ -100,3 +100,46 @@ def test_close_prs_for_deleted_commits_returns_remaining_prs() -> None:
     open_prs = push.close_prs_for_deleted_commits(github_env, prs, commits)
     # then
     assert open_prs == [pr_to_keep]
+
+
+def test_reordered_when_too_many_commits() -> None:
+    # given
+    commits = [Commit("foo", "", "", "", False)]
+    prs = []
+    # when
+    result = push.reordered(prs, commits)
+    # then
+    # for all the PRs, the commits were in the correct order,
+    # there may be additional commits, but those will become
+    # new PRs
+    assert result is False
+
+
+def test_reordered_when_not_reordered() -> None:
+    # given
+    commit_foo = Commit("foo", "", "", "", False)
+    commit_bar = Commit("bar", "", "", "", False)
+    commits = [commit_foo, commit_bar]
+    prs = [
+        PullRequest("", "", "", "", "", "", "", "", "", "", [commit_foo]),
+        PullRequest("", "", "", "", "", "", "", "", "", "", [commit_bar]),
+    ]
+    # when
+    result = push.reordered(prs, commits)
+    # then
+    assert result is False
+
+
+def test_reordered_when_reordered() -> None:
+    # given
+    commit_foo = Commit("foo", "", "", "", False)
+    commit_bar = Commit("bar", "", "", "", False)
+    commits = [commit_foo, commit_bar]
+    prs = [
+        PullRequest("", "", "", "", "", "", "", "", "", "", [commit_bar]),
+        PullRequest("", "", "", "", "", "", "", "", "", "", [commit_foo]),
+    ]
+    # when
+    result = push.reordered(prs, commits)
+    # then
+    assert result is True
