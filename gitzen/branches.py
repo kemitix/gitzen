3816b73d20ffc673b841af6999f84326e6f97e5d
Paul Campbell <pcampbell@kemitix.net>
2022-09-09 07:40:35 +0100

    Use ConsoleEnv in branches
    commit-id:5a63bf16

--- gitzen/branches.py
+++ gitzen/branches.py
@@ -1,6 +1,7 @@
 import re
 
-from gitzen import config, exit_code
+from gitzen import config, envs, exit_code
+from gitzen.console import say
 
 
 # look for the local branch name in the remote branches from config
@@ -13,27 +14,32 @@ def get_remote_branch(local_branch: str, config: config.Config) -> str:
 
 
 def get_required_remote_branch(
+    console_env: envs.ConsoleEnv,
     local_branch: str,
     config: config.Config,
 ) -> str:
     remote_branch = get_remote_branch(local_branch, config)
     if len(remote_branch) == 0:
-        print("remote branch not found")
+        say(console_env, "remote branch not found")
         exit(exit_code.REMOTE_BRANCH_NOT_FOUND)
     return remote_branch
 
 
-def validate_not_remote_pr(local_branch: str) -> None:
+def validate_not_remote_pr(
+    console_env: envs.ConsoleEnv,
+    local_branch: str,
+) -> None:
     matches = re.search(
         # gitzen/pr/{user}/{target-branch}/{zentoken}
         r"gitzen/pr/[a-zA-Z0-9_\-]+/([a-zA-Z0-9_\-/\.]+)/([a-f0-9]{8})$",
         local_branch,
     )
     if matches:
-        print(
+        say(
+            console_env,
             "It looks like you've checked out the remote branch. "
             "You don't need to do that. "
             "Simply update your own local branch. "
-            "Then use `git zen push` to update the pull request."
+            "Then use `git zen push` to update the pull request.",
         )
         exit(exit_code.REMOTE_PR_CHECKED_OUT)
