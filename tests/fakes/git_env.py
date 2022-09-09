0d73b2d4a9b8babefcf5893ec0cfd7bde1ef3c7b
Paul Campbell <pcampbell@kemitix.net>
2022-09-09 19:34:16 +0100

    Add responses to FakeGithubEnv
    commit-id:7752ab5a

--- tests/fakes/git_env.py
+++ tests/fakes/git_env.py
@@ -13,13 +13,15 @@ class FakeGitEnv(envs.GitEnv):
             self.request_counters[args] = 0
 
     def git(self, args: str) -> List[str]:
+        print(f">FakeGit> {args}")
         if args in self.responses:
             counter = self.request_counters[args]
             if len(self.responses[args]) > counter:
                 response = self.responses[args][counter]
                 self.request_counters[args] += 1
+                print(f"<FakeGit< {response}")
                 return response
-            print(f"no more responses for these args: {args}")
+            print(f"ERROR: no more responses for these args: {args}")
         else:
-            print(f"no response for these args: {args}")
+            print(f"ERROR: no response for these args: {args}")
         exit(1)
