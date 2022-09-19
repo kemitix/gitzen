1a2962adaa7492305d36958796a2f4237b545d0c
Your Name <you@example.com>
2022-09-19 09:49:08 +0100

    Add patterns.short_hash
    commit-id:2a449ed2

--- gitzen/patterns.py
+++ gitzen/patterns.py
@@ -1,4 +1,5 @@
 commit_hash = r"[a-f0-9]{40}"
+short_hash = r"[a-f0-9]{7}"
 commit_log_hash = rf"^commit ({commit_hash})"
 zen_token = r"(?P<zen_token>[a-f0-9]{8})"
 commit_body_zen_token = rf"^(zen-token):{zen_token}$"
@@ -8,4 +9,4 @@ branch_name = r"(?P<target_branch>[a-zA-Z0-9_\-/\.]+)"
 remote_pr_branch = rf"^gitzen/pr/{user}/{branch_name}/{zen_token}$"
 remote_patch_branch = rf"^gitzen/patch/{user}/{zen_token}$"
 
-rebase_pick = r"^pick\s+(?P<hash>[a-f0-9]{7})\s+(?P<log>.*)$"
+rebase_pick = rf"^pick\s+(?P<hash>{short_hash})\s+(?P<log>.*)$"
