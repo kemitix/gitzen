f4c799f31854b4d1fab5a66a7040dfe4cba4e3c1
Your Name <you@example.com>
2022-09-19 14:06:28 +0100

    Add push.update_pr()
    commit-id:d0752126

--- tests/test_github_update_pr.py
+++ tests/test_github_update_pr.py
@@ -9,16 +9,23 @@ from . import object_mother as om
 @mock.patch("subprocess.run")
 def test_invokes_command(mock_subproc_run) -> None:
     # given
+    pr_branch = om.gen_git_branch_name()
     base = om.gen_git_branch_name()
     commit = om.gen_commit(token=None)
     # when
-    github.update_pull_request(github.RealGithubEnv(), base, commit)
+    github.update_pull_request(
+        github.RealGithubEnv(),
+        pr_branch,
+        base,
+        commit,
+    )
     # then
     mock_subproc_run.assert_called_with(
         [
             "gh",
             "pr",
             "edit",
+            pr_branch.value,
             "--base",
             base.value,
             "--title",
