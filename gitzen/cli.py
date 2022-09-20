abc9efc05c3b606035471c33fc30940ab073a6a5
Your Name <you@example.com>
2022-09-20 19:21:24 +0100

    Clean up cli
    commit-id:f9d9e0ad

--- gitzen/cli.py
+++ gitzen/cli.py
@@ -7,23 +7,17 @@ from gitzen.console import RealConsoleEnv
 
 def main(args: List[str]) -> None:
     console_env: console.Env = RealConsoleEnv()
-
     git_env = git.RealGitEnv()
-
     # verify that we are in a git repo or exit
     root_dir = git.root_dir(console_env, git_env)
-
-    github_env = github.RealGithubEnv()
     cfg = config.load(console_env, root_dir)
-
-    for i, arg in enumerate(args):
-        if i != 1:
-            continue
-        if arg == "init":
-            init.install_hook(console_env, root_dir)
-        if arg == "hook":
-            hook.main(args[2:])
-        if arg == "status":
-            status.status(console_env, git_env, github_env)
-        if arg == "push":
-            push.push(console_env, git_env, github_env, cfg)
+    github_env = github.RealGithubEnv()
+    arg = args[1]
+    if arg == "init":
+        init.install_hook(console_env, root_dir)
+    if arg == "hook":
+        hook.main(args[2:])
+    if arg == "status":
+        status.status(console_env, git_env, github_env)
+    if arg == "push":
+        push.push(console_env, git_env, github_env, cfg)
