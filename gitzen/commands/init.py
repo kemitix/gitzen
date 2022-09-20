de368d1dfe4a064570e665d2f4940dee8631947a
Your Name <you@example.com>
2022-09-20 07:35:38 +0100

    Make hook executable during init
    commit-id:d3ac33e9

--- gitzen/commands/init.py
+++ gitzen/commands/init.py
@@ -1,15 +1,20 @@
+import os
+import stat
+
 from gitzen import envs, file
 from gitzen.console import info
 from gitzen.types import GitRootDir
 
 
 def install_hook(console_env: envs.ConsoleEnv, root_dir: GitRootDir) -> None:
+    hook = f"{root_dir.value}/.git/hooks/commit-msg"
     file.write(
-        f"{root_dir.value}/.git/hooks/commit-msg",
+        hook,
         [
             "#!/usr/bin/env bash",
             "git zen hook $1",
             "",
         ],
     )
+    os.chmod(hook, os.stat(hook).st_mode | stat.S_IEXEC)
     info(console_env, "Installed git zen hook")
