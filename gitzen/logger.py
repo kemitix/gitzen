3f6e34ebd2cba113f4a82b0edf2e8989e795da86
Your Name <you@example.com>
2022-09-21 17:53:11 +0100

    Add logger
    commit-id:bbbfca1c

---
+++ gitzen/logger.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Env:
+    def log(self, section: str, message: str) -> None:
+        pass
+
+
+class RealEnv(Env):
+    sections: List[str]
+
+    def __init__(self, sections: List[str] = ["all"]) -> None:
+        super().__init__()
+        self.sections = sections
+
+    def _log(self, section: str, message: str) -> None:
+        if "all" in self.sections or section in self.sections:
+            log_line = f"{section}> {message}"
+            print(log_line)
+
+
+def log(logger_env: Env, section: str, message: str) -> None:
+    logger_env.log(section, message)
