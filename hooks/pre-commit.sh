69b2cbc97df7a507db4ae9edc9ed5241da989da1
Your Name <you@example.com>
2022-09-22 06:57:40 +0100

    workflow,pre-commit: run high value tests first
    Formatting tests are pushed to last as they are trivial fixes, while
    bringing type-consistency and tests to the front for quicker
    feedback. Quicker as the requirements for trunk take a time to
    install, so don't do that if there are more important problems to be
    fixed first..
    
    commit-id:1ea0675b

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo -e "\n> trunk fmt"
-trunk fmt --ci
-echo -e "\n>trunk check"
-trunk check --ci
 echo -e "\n> pyre infer"
 pyre infer --in-place --annotate-attributes
 echo -e "\n> pyre check"
 pyre check
 echo -e "\n> pytest"
 pytest --cov=. --cov-branch --cov-report=xml
+echo -e "\n> trunk fmt"
+trunk fmt --ci
+echo -e "\n>trunk check"
+trunk check --ci
 echo -e "\n< Done\n"
