d004126dd0fb1368e5b23a635ba6c3f4997b27b7
Paul Campbell <pcampbell@kemitix.net>
2022-09-25 20:04:34 +0100

    Merge pr checking hash against latest commit
    zen-token:02084f98
    (updated from commit f7787b5049a84336e364fa5bf06c2e01c84be4d8)

--- gitzen/git.py
+++ gitzen/git.py
@@ -291,6 +291,29 @@ def push(
     return log
 
 
+def push_force_with_lease(
+    git_env: Env,
+    remote: GitRemoteName,
+    branch: GitBranchName,
+) -> List[str]:
+    rc, log = git_env._git(
+        (
+            "push "
+            f"--force-with-lease {remote.value} "
+            f"{branch.value}:{branch.value}"
+        ),
+    )
+    if rc:
+        raise GitZenError(
+            rc,
+            (
+                f"Unable to force push changes to remote {remote.value} "
+                f"from local branch {branch.value}"
+            ),
+        )
+    return log
+
+
 def rebase(
     git_env: Env,
     target: GitBranchName,
