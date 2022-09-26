39a2fe325d563ce7ec57d0af240c5ff34c9bc988
Paul Campbell <pcampbell@kemitix.net>
2022-09-26 07:15:36 +0100

    delete old pr branch when merged
    zen-token:bc7fb401
    (updated from commit 8534878020ec4a171f2a9b26225f78eeaff32ad5)

--- gitzen/commands/push.py
+++ gitzen/commands/push.py
@@ -112,7 +112,6 @@ def prepare_patches(
     update_patches(console_env, file_env, cfg.root_dir, commits)
     commit_stack = clean_up_deleted_commits(
         console_env,
-        git_env,
         github_env,
         status.pull_requests,
         commits,
@@ -175,7 +174,6 @@ def rethread_stack(
 
 def clean_up_deleted_commits(
     console_env: console.Env,
-    git_env: git.Env,
     github_env: github.Env,
     pull_requests: List[PullRequest],
     commits: List[GitCommit],
@@ -201,8 +199,6 @@ def clean_up_deleted_commits(
                 pr,
                 "Closing pull request: commit has gone away",
             )
-            if git.branch_exists(git_env, pr.headRefName):
-                git.branch_delete(git_env, pr.headRefName)
             git.delete_patch(pr.zen_token, root_dir)
         else:
             commit = zen_tokens[pr.zen_token]
