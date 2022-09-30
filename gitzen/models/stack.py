8002d1a1cc2dbf5cdd54d0a868f224610b679264
Paul Campbell <pcampbell@kemitix.net>
2022-09-30 06:47:08 +0100

    Add access to commits in Stack by ZenToken
    zen-token:1e49e6ed
    
    commit-id:c675d0cb

--- gitzen/models/stack.py
+++ gitzen/models/stack.py
@@ -1,16 +1,34 @@
 import copy
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from gitzen.models.git_commit import GitCommit
-from gitzen.types import GitBranchName
+from gitzen.types import GitBranchName, ZenToken
 
 
 class Stack:
     _branches: Dict[GitBranchName, List[GitCommit]]
+    _tokens: Dict[ZenToken, GitCommit]
 
     def __init__(self, branches: Dict[GitBranchName, List[GitCommit]]) -> None:
         self._branches = branches
+        tokens = {}
+        for branch in branches:
+            for commit in branches[branch]:
+                tokens[commit.zen_token] = commit
+        self._tokens = tokens
+
+    def branch(self, branch: GitBranchName) -> List[GitCommit]:
+        if branch in self._branches:
+            return copy.deepcopy(self._branches[branch])
+        return []
 
     @property
     def branches(self) -> Dict[GitBranchName, List[GitCommit]]:
         return copy.deepcopy(self._branches)
+
+    def token(self, token: ZenToken) -> Optional[GitCommit]:
+        return self._tokens.get(token)
+
+    @property
+    def tokens(self) -> Dict[ZenToken, GitCommit]:
+        return copy.deepcopy(self._tokens)
